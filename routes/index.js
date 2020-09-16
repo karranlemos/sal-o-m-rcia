7bf29150ad467bd8bc2906abe2e2a3078078fb67
Karran Lemos <karranlemos@gmail.com>
2020-09-15 21:34:38 -0300

    Add overlay with title and text; refactor main-header code

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,43 @@ router.use(express.static('public'));
 
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        headerContent: {
+            page: 'home',
+            title: 'Salão da Márcia',
+            text: 'Exaltando a sua beleza natural.'
+        }
+    });
 });
 
 router.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {
+        headerContent: {
+            page: 'about',
+            title: 'Sobre Nós',
+            text: 'Somos um salão de beleza especializado em exaltar a sua beleza com um sorriso no rosto.'
+        }
+    });
 });
 
 router.get('/services', (req, res) => {
-    res.render('services');
+    res.render('services', {
+        headerContent: {
+            page: 'services',
+            title: 'Nossos Serviços',
+            text: 'A nossa lista de serviços tem tudo o que você poderia procurar.'
+        }
+    });
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        headerContent: {
+            page: 'contact',
+            title: 'Contato',
+            text: 'Contate-nos!'
+        }
+    });
 });
 
 
